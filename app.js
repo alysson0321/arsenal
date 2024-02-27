0818c37228334f55be050b28e6c65dc6960bcca9
alysson0321 <alyssonfdbr874@gmai.com>
2024-02-27 16:46:44 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+function abrirpag(){
+    window.location.href = 'https://pt.wikipedia.org/wiki/Arsenal_Football_Club';
+}
+function abrirpag2(){
+    window.location.href = "pag2.html";
+}
+function voltar(){
+    window.location.href = "index.html"
+}
