933ab7d09a1f7abc5b08fae3769ae14d47a5fb74
alysson0321 <alyssonfdbr874@gmai.com>
2024-04-08 19:41:53 -0300

    nomes dos arquivos html alterados

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 function abrirpag(){
-    window.location.href = "pag3.html";
+    window.location.href = "historia.html";
 }
 function abrirpag2(){
-    window.location.href = "pag2.html";
+    window.location.href = "jogadores.html";
 }
 function voltar(){
     window.location.href = "pag1.html";
